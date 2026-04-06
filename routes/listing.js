112fbd7caf2725a6f73356f8006237143504491d
Vikash Kumar <178039979+vikashkr96@users.noreply.github.com>
2026-04-06 22:37:59 +0530

    Adapting MVC design pattern in our project- controllers

--- routes/listing.js
+++ routes/listing.js
@@ -5,13 +5,11 @@ const Listing = require("../models/listing.js");
 const {isLoggedIn , isOwner, validateListing} = require("../middleware.js");
 
 
-
+const listingController = require("../controllers/listing.js") ;
 
 // index route
-router.get("/", wrapAsync(async (req, res) => {
-    const allListings = await Listing.find();
-    res.render("listings/index.ejs", { allListings });
-}));
+router.get("/", wrapAsync(listingController.index));
+
 
 // new route
 router.get("/new", isLoggedIn, (req, res) => {
