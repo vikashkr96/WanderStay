2e796f153a6c1ae836d9eddf5953f578a8b34693
Vikash Kumar <178039979+vikashkr96@users.noreply.github.com>
2026-04-14 21:50:49 +0530

    map related functionality completed!

--- controllers/listing.js
+++ controllers/listing.js
@@ -1,4 +1,7 @@
 const Listing = require("../models/listing");
+const mbxGeocoding = require("@mapbox/mapbox-sdk/services/geocoding");
+const mapToken = process.env.MAP_TOKEN;
+const geocodingClient = mbxGeocoding ({accessToken : mapToken});
 
 module.exports.index = async (req, res) => {
     const allListings = await Listing.find();
@@ -23,12 +26,21 @@ module.exports.showListing = async (req, res) => {
 } ;
 
 module.exports.createListing = async (req, res, next) => {
+    let response = await geocodingClient.forwardGeocode({
+        query: req.body.listing.location,
+        limit: 1
+    })
+    .send();
+
     let url = req.file.path;
     let filename = req.file.filename;
     const newListing = new Listing(req.body.listing);
     newListing.owner = req.user._id;
     newListing.image = {url, filename};
-    await newListing.save();
+
+    newListing.geometry = response.body.features[0].geometry;
+    let savedListing = await newListing.save();
+
     req.flash("success", "New Listing Created!");
     res.redirect("/listings");
 };
@@ -47,8 +59,16 @@ module.exports.renderEditForm = async (req, res) => {
 };
 
 module.exports.updateListing = async(req, res) => {
+    let response = await geocodingClient.forwardGeocode({
+        query: req.body.listing.location,
+        limit: 1
+    })
+    .send();
+
     let { id } = req.params;
     let listing = await Listing.findByIdAndUpdate(id, { ...req.body.listing });
+    listing.geometry = response.body.features[0].geometry;
+    await listing.save();
 
     if(typeof req.file !== "undefined"){
         let url = req.file.path;
--- public/js/map.js
+++ public/js/map.js
@@ -3,10 +3,30 @@ mapboxgl.accessToken = mapToken;
 const map = new mapboxgl.Map({
     container: 'map',
     style: 'mapbox://styles/mapbox/streets-v12',
-    center: [77.2088, 28.6139],
+    center: listing.geometry.coordinates,
     zoom: 9
 });
 
 map.addControl(new mapboxgl.NavigationControl(), 'top-right');
 map.addControl(new mapboxgl.FullscreenControl(), 'top-right');
 map.scrollZoom.disable();
+
+
+map.on('load', () => {
+  const popup = new mapboxgl.Popup({ offset: 25, closeButton: false })
+    .setHTML(`<h4>${listing.title}</h4><p>Exact location will be provided after booking</p>`);
+
+  const marker = new mapboxgl.Marker({ color: "red" })
+    .setLngLat(listing.geometry.coordinates)
+    .addTo(map);
+
+  marker.getElement().addEventListener('mouseenter', () => {
+    popup.setLngLat(listing.geometry.coordinates).addTo(map);
+  });
+
+  marker.getElement().addEventListener('mouseleave', () => {
+    popup.remove();
+  });
+
+  map.flyTo({ center: listing.geometry.coordinates, zoom: 8 });
+});
