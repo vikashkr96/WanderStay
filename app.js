418887e376463ff647a8c02fc99d0fda2b1cc98f
Vikash Kumar <178039979+vikashkr96@users.noreply.github.com>
2026-04-25 20:06:09 +0530

    minor change

--- app.js
+++ app.js
@@ -46,7 +46,7 @@ async function main() {
 const store = MongoStore.create({
     mongoUrl:dbURL,
     crypto:{
-        secret:"mysupersecretcode"
+        secret:process.env.SECRET
     },
     touchAfter: 24*3600,
 })
@@ -54,7 +54,7 @@ console.log(MongoStore);
 // session options 
 const sessionOption = {
     store ,
-    secret: "mysupersecretcode",
+    secret: process.env.SECRET,
     resave:false,
     saveUninitialized: true,
     cookie: {
